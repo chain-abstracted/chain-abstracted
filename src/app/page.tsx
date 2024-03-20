62f614da2c346f7be6cbf5e1609fbc0fcd4e4163
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2024-03-20 09:46:50 +0000

    Support mobile.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-row w-[100vw] h-[100vh] gap-10 lg:gap-20">
-      <div className="flex flex-col w-[50%] justify-center pl-[10%] lg:pl-[20%] gap-3">
-        <Image src={logo} alt="Picture of the author" className="w-[40%]" />
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <div className="flex flex-wrap lg:flex-nowrap lg:h-[100vh] w-100% gap-10 lg:gap-20">
+      <div className="w-full lg:w-1/2 px-4 flex flex-col justify-center lg:pl-[20%] gap-3">
+        <Image
+          src={logo}
+          alt="Picture of the author"
+          className="w-[40%] mx-auto lg:mx-0"
+        />
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           Chain Abstracted Podcast
         </h1>
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        <h3 className="text-2xl font-semibold tracking-tight">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </h3>
         <p className="text-justify">
@@ -27,12 +31,8 @@ export default function Home() {
           augue a eleifend volutpat. Interdum et malesuada fames ac ante ipsum
           primis in faucibus.
         </p>
-        {/* <div className="flex flex-row gap-3">
-          <Input placeholder="Email" />
-          <Button>Subscribe</Button>
-        </div> */}
       </div>
-      <div className="flex flex-col w-[50%] justify-center pr-[10%] lg:pr-[20%] gap-3">
+      <div className="w-full lg:w-1/2 px-4 flex flex-col justify-center lg:pr-[20%] gap-3 mb-10">
         <iframe
           src="https://open.spotify.com/embed/album/18NOKLkZETa4sWwLMIm0UZ?utm_source=generator"
           width="100%"
